890bfc8d27e061e5becbd2a83c100a91632168c3
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-28 03:40:06 +0800

    Removed unused sound file

--- Salami/Sounds.py
+++ Salami/Sounds.py
@@ -7,7 +7,6 @@ HURT = arcade.load_sound("resources/hurt.wav")
 JUMP = arcade.load_sound("resources/jump.wav")
 SHOOT = arcade.load_sound("resources/shoot.wav")
 SLIME_JUMP = arcade.load_sound("resources/slime_jump.wav")
-BOSS_INTRO = arcade.load_sound("resources/boss_intro.wav")
 SKELETON_HIT = arcade.load_sound("resources/skeleton_hit.wav")
 
 def play(sound):
